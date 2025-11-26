852f5dff57cb40ed443dc4f22f43f3497ca25de2
DongShaoNB <54093566+DongShaoNB@users.noreply.github.com>
2025-11-26 19:18:32 +0800

    feat: try to support EdgeOne

---
+++ install_for_edgeone.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VER=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep -o '"tag_name": *"v[^"]*"' | cut -d'"' -f4)
+
+NUM=${VER#v}
+
+curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/${VER}/hugo_extended_${NUM}_Linux-64bit.tar.gz
+
+tar -xzf hugo.tar.gz
+chmod +x hugo
+mv hugo /usr/local/bin/
